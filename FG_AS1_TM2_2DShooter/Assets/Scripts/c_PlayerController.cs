5b3e95f4f10e3f6fc45c7d3657469355696ea33e
DutchDave <dave.van_egdom@futuregames.nu>
2021-09-08 21:06:28 +0200

    Added some scripts

--- FG_AS1_TM2_2DShooter/Assets/Scripts/c_PlayerController.cs
+++ FG_AS1_TM2_2DShooter/Assets/Scripts/c_PlayerController.cs
@@ -4,10 +4,52 @@ using UnityEngine;
 
 public class c_PlayerController : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    private Vector2 moveDirection;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float defaultSpeed;
+    [SerializeField] float turnRate;
+    [SerializeField] float defaultTurnRate;
+
+    [SerializeField] float boostMultiplier;
+    [SerializeField] float defaultBoostMultiplier;
+
+    [SerializeField] float warpDistance;
+    [SerializeField] float defaultWarpDistance;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world");
+        
     }
+
+    private void Update()
+    {
+        //Input system
+        #region Input
+
+        //Move forward
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //Move
+        }
+
+
+        //Shoot the gun when the left mouse button is clicked
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Shoot");
+        }
+
+        //Focus fire by pressing the right mouse button
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Focus fire"); //Zoom camera in?
+        }
+
+        #endregion
+    }
+
 
 }
